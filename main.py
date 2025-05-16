249aa132f2ad9ef70aa061cf480c98d0ae17951e
Deadstare0-0 <ktembeleski@gmail.com>
2025-05-15 21:30:28 -0400

    updated main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    count = count_words(book_path)
+    print(f"{count} words found in the document")
+
+def count_words(path):
+    with open(path) as f:
+        temp = f.read()
+    count = len(temp.split())
+    return count
+
+main()
